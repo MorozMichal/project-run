c59495e76909679ce8807ac080308329fea0df18
Michał Moroz <moroz_michal@o2.pl>
2019-01-31 23:09:17 +0100

    work section running

--- js/running.js
+++ js/running.js
@@ -1,50 +1,67 @@
 const imgArms = document.querySelectorAll('.running-arm img');
 const h2Cities = document.querySelectorAll('.running-city');
-const arms = document.querySelectorAll('.running-arms')
-const armsWrapper = document.querySelector('.section-running-wrapper')
 const runningContest = document.querySelector('.running-contest')
 
 h2Cities.forEach(function (h2City, index) {
     h2City.addEventListener("mouseover", function () {
         for (let i = 0; i < imgArms.length; i++) {
             imgArms[index].style.animation = "arm 2s infinite linear"
-            this.style.cursor = "pointer"
 
         }
+        h2City.addEventListener("mouseout", function () {
+            for (let i = 0; i < imgArms.length; i++) {
+                imgArms[i].style.animation = "arm 0s infinite linear"
 
-    })
+            }
 
-    h2City.addEventListener("mouseout", function () {
-        for (let i = 0; i < imgArms.length; i++) {
-            imgArms[i].style.animation = "arm 0s infinite linear"
+        })
 
-        }
 
     })
 
     h2City.addEventListener("click", function () {
-
-        for (let i = 0; i < arms.length; i++) {
-            arms[i].style.transform = "translateX(0%)"
-            arms[i].style.transition = "3s"
-            arms[i].style.flexBasis = "35%"
-        }
-        armsWrapper.style.transform = "translateX(0%)"
-        armsWrapper.style.transition = "3s"
-        armsWrapper.style.flexBasis = "35%"
-
-
-        runningContest.style.display = "inline-block"
-        runningContest.style.transform = "translateX(200%)"
         runningContest.style.transform = "translateX(0%)"
-        runningContest.style.flexBasis = "75%"
-        runningContest.style.transition = "10s"
-
-
+        runningContest.style.transition = "3s"
         this.style.backgroundColor = "silver"
         this.style.transition = "2s";
 
-
-
     })
 })
+
+
+
+
+
+//funkcja chowania i zmiany kolejnosci wyswietlania divo poszczegolnch miast
+function reset() {
+    const noActive = document.querySelectorAll(".no-active");
+    for (let i = 0; i < noActive.length; i++) {
+        noActive[i].style.order = "1"; //nadanie wszystkim div kolejnosci 
+        noActive[i].style.transform = "translateX(150%)" //chowa posotałe divy na prawo poza stronę
+        noActive[i].style.transition = "0.2s linear"; //czas chowania sie diva w prawa strone
+
+    }
+}
+
+//funkcja przesuwa elementy przy pierwszym zaladowaniu strony
+function showContest() {
+    const SectionContestContest = document.querySelector(".running-contest");
+    SectionContestContest.style.transition = "0.5s linear"; //czas wjazdu opisu w lewa strone
+    SectionContestContest.style.transform = "translateX(0%)"; // przesuniecie sie opisu do pozycji 0
+
+}
+
+const buttonAllCity = [...document.querySelectorAll(".running-arms>h2")];
+
+//funkcja nastawia nasłuchiwanie na każdą nazwę miasta na mapie poprawnia właściwości w tablicy AllConstens za pomoca indexku buttona kolejnośc dodania buttona i miasta ma byc taka sama
+buttonAllCity.forEach(function (buttonCity, index) {
+    buttonCity.addEventListener("click", function () {
+        const AllContents = [...document.querySelectorAll(".running-contest>div")];
+        reset();
+        showContest();
+        AllContents[index].style.order = "-1";
+        AllContents[index].style.transform = "translateX(0%)";
+        AllContents[index].style.transition = "1s linear";
+
+    })
+});
