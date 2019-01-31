e11475b14db8cf332e7774d6163f199af0f19303
MorozMichal <34334015+MorozMichal@users.noreply.github.com>
2019-01-31 12:05:39 +0100

    code js/css section running

--- js/running.js
+++ js/running.js
@@ -1,10 +1,10 @@
 const imgArms = document.querySelectorAll('.running-arm img');
 const h2Cities = document.querySelectorAll('.running-city');
-const arms = document.querySelector('.section-running-wrapper')
+const arms = document.querySelectorAll('.running-arms')
+const armsWrapper = document.querySelector('.section-running-wrapper')
 
 h2Cities.forEach(function (h2City, index) {
     h2City.addEventListener("mouseover", function () {
-        console.log("działa")
         for (let i = 0; i < imgArms.length; i++) {
             imgArms[index].style.animation = "arm 2s infinite linear"
             this.style.cursor = "pointer"
@@ -14,7 +14,6 @@ h2Cities.forEach(function (h2City, index) {
     })
 
     h2City.addEventListener("mouseout", function () {
-        console.log("działa")
         for (let i = 0; i < imgArms.length; i++) {
             imgArms[i].style.animation = "arm 0s infinite linear"
 
@@ -23,8 +22,17 @@ h2Cities.forEach(function (h2City, index) {
     })
 
     h2City.addEventListener("click", function () {
-        arms.style.transition = "2s"
-        arms.style.left = "0"
+
+        for (let i = 0; i < arms.length; i++) {
+            // arms[i].style.flexBasis = "55%"
+            // arms[i].style.flexWrap = "nowrap"
+            // arms[i].style.justifyContent = "left"
+            arms[i].style.transform = "translateX(0%)"
+            arms[i].style.transition = "2s"
+            arms[i].style.flexBasis = "45%"
+        }
+        armsWrapper.style.transform = "translateX(0%)"
+        armsWrapper.style.transition = "2s"
         this.style.backgroundColor = "silver"
         this.style.transition = "2s";
 
