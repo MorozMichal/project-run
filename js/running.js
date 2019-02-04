f5b8d873eb315bf501461443cee228a6eb881bce
MorozMichal <34334015+MorozMichal@users.noreply.github.com>
2019-02-04 11:51:48 +0100

    chanhe links

--- js/running.js
+++ js/running.js
@@ -2,19 +2,19 @@ const imgArms = document.querySelectorAll(".running-arm img");
 const h2Cities = document.querySelectorAll(".running-city");
 const runningContest = document.querySelector(".running-contest");
 
-h2Cities.forEach(function(h2City, index) {
-  h2City.addEventListener("mouseover", function() {
+h2Cities.forEach(function (h2City, index) {
+  h2City.addEventListener("mouseover", function () {
     for (let i = 0; i < imgArms.length; i++) {
       imgArms[index].style.animation = "arm 2s infinite linear";
     }
-    h2City.addEventListener("mouseout", function() {
+    h2City.addEventListener("mouseout", function () {
       for (let i = 0; i < imgArms.length; i++) {
         imgArms[i].style.animation = "arm 0s infinite linear";
       }
     });
   });
 
-  h2City.addEventListener("click", function() {
+  h2City.addEventListener("click", function () {
     runningContest.style.transform = "translateX(0%)";
     runningContest.style.transition = "3s";
     this.style.backgroundColor = "silver";
@@ -42,8 +42,8 @@ function showContest() {
 const buttonAllCity = [...document.querySelectorAll(".running-arms>h2")];
 
 //funkcja nastawia nasłuchiwanie na każdą nazwę miasta na mapie poprawnia właściwości w tablicy AllConstens za pomoca indexku buttona kolejnośc dodania buttona i miasta ma byc taka sama
-buttonAllCity.forEach(function(buttonCity, index) {
-  buttonCity.addEventListener("click", function() {
+buttonAllCity.forEach(function (buttonCity, index) {
+  buttonCity.addEventListener("click", function () {
     const AllContents = [...document.querySelectorAll(".running-contest>div")];
     reset();
     showContest();
@@ -55,8 +55,8 @@ buttonAllCity.forEach(function(buttonCity, index) {
 
 const runningContestNames = document.querySelectorAll(".contest-name");
 
-runningContestNames.forEach(function(e) {
-  e.addEventListener("click", function() {
+runningContestNames.forEach(function (e) {
+  e.addEventListener("click", function () {
     this.nextElementSibling.classList.toggle("novisible");
   });
 });
