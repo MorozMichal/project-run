c44610b770cd51b40579b86b7b43af6737ddfa20
Michał Moroz <moroz_michal@o2.pl>
2019-02-06 20:36:11 +0100

    script new previews section

--- js/previews.js
+++ js/previews.js
@@ -9,3 +9,17 @@ previews.forEach(function (preview, index) {
         previewContent[index].style.opacity = "1"
     })
 })
+
+
+
+const btnShowPreviews = document.querySelector('.section-previews-banner i')
+// const btnBanner = document.querySelector('.section-previews-banner')
+const previewsAllRacings = document.querySelector('.section-previews-racing')
+
+
+btnShowPreviews.addEventListener('click', function () {
+    previewsAllRacings.classList.toggle('section-previews-racing-visible')
+    btnShowPreviews.classList.toggle('fa-angle-right')
+    btnShowPreviews.classList.toggle('fa-angle-left')
+
+})
