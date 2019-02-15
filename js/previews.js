b488392a43c9670139797f6242b3316457c20dfa
MorozMichal <34334015+MorozMichal@users.noreply.github.com>
2019-02-15 11:22:13 +0100

    modify previews.js =>

--- js/previews.js
+++ js/previews.js
@@ -1,29 +1,24 @@
-const previews = [...document.querySelectorAll('.preview')]
-const previewContent = [...document.querySelectorAll('.preview-content')]
-
-previews.forEach(function (preview, index) {
-    preview.addEventListener("click", function () {
-        for (let i = 0; i < previewContent.length; i++) {
-            previewContent[i].style.opacity = "0"
-        }
-        previewContent[index].style.opacity = "1"
-    })
-})
-
-
-
+const previews = document.querySelectorAll('.preview')
+const previewContents = document.querySelectorAll('.preview-content')
 const btnShowPreviews = document.querySelector('.section-previews-banner i')
 const previewsAllRacings = document.querySelector('.section-previews-racing')
 
-btnShowPreviews.addEventListener('click', function () {
+//pokazywanie i chowanie bocznego panelu z zapowiedziami
+btnShowPreviews.addEventListener('click', () => {
     previewsAllRacings.classList.toggle('section-previews-racing-visible')
     btnShowPreviews.classList.toggle('fa-angle-right')
     btnShowPreviews.classList.toggle('fa-angle-left')
     btnShowPreviews.classList.toggle('animation-previews-button')
-    if (btnShowPreviews.classList.contains('fa-angle-left')) { //sprawdzenie czy element posiada daną klasę
-        for (let i = 0; i < previewContent.length; i++) {
-            previewContent[i].style.opacity = "0"
-        }
+    if (btnShowPreviews.classList.contains('fa-angle-left')) { //sprawdzenie czy element ma daną klasę
+        previewContents.forEach(previewContent => previewContent.style.opacity = "0")
     }
 
+})
+
+//pokazywanie opisu poszczególnych zapowiedzi
+previews.forEach((preview, index) => {
+    preview.addEventListener("click", () => {
+        previewContents.forEach(previewContent => previewContent.style.opacity = "0")
+        previewContents[index].style.opacity = "1"
+    })
 })
