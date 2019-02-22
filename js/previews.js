a59f1238e5516d2cc09183f1daf8d5e895d1dc8c
MorozMichal <34334015+MorozMichal@users.noreply.github.com>
2019-02-22 15:13:21 +0100

    preview add addContent metod

--- js/previews.js
+++ js/previews.js
@@ -1,5 +1,118 @@
+class PreviewCity {
+    constructor(city, date, description, content) {
+        this.city = city;
+        this.date = date;
+        this.description = description;
+        this.content = content
+        this.previewsAll = document.querySelector(".section-previews-all");
+        this.previewContents = document.querySelector('.preview-content')
+    }
+
+    addPreview() {
+        const divPreview = document.createElement('div'); //zbiorczy div dla 3*p
+        divPreview.classList.add("preview")
+        this.previewsAll.appendChild(divPreview)
+
+        const previewPcity = document.createElement('p'); //p z nazwą miasta
+        divPreview.appendChild(previewPcity)
+
+        const previewPdate = document.createElement('p'); //p z data zawodów
+        divPreview.appendChild(previewPdate)
+
+        const previewPdescription = document.createElement('p'); //p z nazwa biegu
+        divPreview.appendChild(previewPdescription)
+
+        previewPcity.textContent = `${this.city}`;
+        previewPdate.textContent = `${this.date}`;
+        previewPdescription.textContent = `${this.description}`;
+    }
+
+    addContent() {
+        this.previewContents.textContent = `${this.content}`
+        this.previewContents.style.opacity = "1"
+    }
+}
+
+const arrayPreview = []
+
+const laskowice311218 = new PreviewCity('Jelcz-Laskowice', '31.12.2018r.', 'IV TOYOTA Bieg Sylwestrowy', 'Jelcz-Laskowice')
+arrayPreview.push(laskowice311218)
+// laskowice311218.addPreview()
+
+const sobotka230319 = new PreviewCity('Sobótka', '23.03.2019r.', '12. PANAS Pólmaraton Ślężański', 'Sobótka')
+arrayPreview.push(sobotka230319)
+// sobotka230319.addPreview()
+
+const krakow240319 = new PreviewCity('Kraków', '24.03.2019r.', '16 Krakowski Półmaraton Marzanny', 'Kraków')
+arrayPreview.push(krakow240319)
+// krakow240319.addPreview()
+
+const warszawa310319 = new PreviewCity('Warszawa', '31.03.2019r.', '14 Półmaraton Warszawski', 'Warszawa')
+arrayPreview.push(warszawa310319)
+// warszawa310319.addPreview()
+
+const debno070419 = new PreviewCity('Dębno', '07.04.2019r.', '46. Maraton Dębno', 'Dębno')
+arrayPreview.push(debno070419)
+// debno070419.addPreview()
+
+const gdansk140419 = new PreviewCity('Gdańsk', '14.04.2019r.', '5. Gdańsk Maraton', 'Gdańsk')
+arrayPreview.push(gdansk140419)
+// gdansk140419.addPreview()
+
+const warszawa140419 = new PreviewCity('Warszawa', '14.04.2019r.', 'Orlen Warsaw Marathon', 'Warszawa')
+arrayPreview.push(warszawa140419)
+// warszawa140419.addPreview()
+
+const poznan140419 = new PreviewCity('Poznań', '14.04.2019r.', '12.PKO Poznań Pólmaraton', 'Poznań')
+arrayPreview.push(poznan140419)
+// poznan140419.addPreview()
+
+const krakow280419 = new PreviewCity('Kraków', '28.04.2019r.', '18. Cracovia Maraton', 'Kraków')
+arrayPreview.push(krakow280419)
+// krakow280419.addPreview()
+
+const poznan050519 = new PreviewCity('Poznań', '05.05.2019r.', 'Wings For Life WorldRun', 'Poznań')
+arrayPreview.push(poznan050519)
+// poznan050519.addPreview()
+
+const wroclaw150619 = new PreviewCity('Wrocław', '15.06.2019r.', '7. PKO Nocny Wrocław Półmaraton', 'Wrocław')
+arrayPreview.push(wroclaw150619)
+// wroclaw150619.addPreview()
+
+const olawa010919 = new PreviewCity('Oława', '01.09.2019r.', 'XII Bieg Koguta', 'Oława')
+arrayPreview.push(olawa010919)
+// olawa010919.addPreview()
+
+const wroclaw150919 = new PreviewCity('Wrocław', '15.09.2019r.', '37. PKO Wrocław Maraton', 'Wrocław')
+arrayPreview.push(wroclaw150919)
+// wroclaw150919.addPreview()
+
+const warszawa290919 = new PreviewCity('Warszawa', '29.09.2019r.', 'PZU Maraton Warszawski', 'Warszawa')
+arrayPreview.push(warszawa290919)
+// warszawa290919.addPreview()
+
+const katowice061019 = new PreviewCity('Katowice', '06.10.2019r.', '11. PKO Silesia Marathon', 'Katowice')
+arrayPreview.push(katowice061019)
+// katowice061019.addPreview()
+
+const poznan201019 = new PreviewCity('Poznań', '20.10.2019r.', '20. PKO Poznań Maraton', 'Poznań')
+arrayPreview.push(poznan201019)
+// poznan201019.addPreview()
+
+arrayPreview.forEach(preview => preview.addPreview()) //dodanie wszystkim elementom tablicy metody addPreview
+
+const previewContent = document.querySelector('.preview-content')
+
 const previews = document.querySelectorAll('.preview')
-const previewContents = document.querySelectorAll('.preview-content')
+previews.forEach((preview, index) => {
+    preview.addEventListener("click", () => {
+        arrayPreview[index].addContent()
+    })
+})
+
+/* ---------------------------------------------------------------------------- */
+// const previews = document.querySelectorAll('.preview')
+// const previewContents = document.querySelectorAll('.preview-content')
 const btnShowPreviews = document.querySelector('.section-previews-banner i')
 const previewsAllRacings = document.querySelector('.section-previews-racing')
 
@@ -15,91 +128,16 @@ btnShowPreviews.addEventListener('click', () => {
 
 })
 
-//pokazywanie opisu poszczególnych zapowiedzi
-previews.forEach((preview, index) => {
-    preview.addEventListener("click", () => {
-        console.log('klik')
-        previewContents.forEach(previewContent => previewContent.style.opacity = "0")
-        previewContents[index].style.opacity = "1"
-    })
-})
+/* ---------------------------------------------------------------------------- */
+// //pokazywanie opisu poszczególnych zapowiedzi
+// previews.forEach((preview, index) => {
+//     preview.addEventListener("click", () => {
+//         previewContents.forEach(previewContent => previewContent.style.opacity = "0")
+//         previewContents[index].style.opacity = "1"
+//     })
+// })
 
-const previewsAll = document.querySelector(".section-previews-all");
 
-class PreviewCity {
-    constructor(city, date, description) {
-        this.city = city;
-        this.date = date;
-        this.description = description;
-    }
-
-    addPreview() {
-        const divPreview = document.createElement('div'); //zbiorczy div dla 3*p
-        divPreview.classList.add("preview")
-        previewsAll.appendChild(divPreview)
-
-        const previewPcity = document.createElement('p'); //p z nazwą miasta
-        divPreview.appendChild(previewPcity)
-
-        const previewPdate = document.createElement('p'); //p z data zawodów
-        divPreview.appendChild(previewPdate)
-
-        const previewPdescription = document.createElement('p'); //p z nazwa biegu
-        divPreview.appendChild(previewPdescription)
-
-        previewPcity.textContent = `${this.city}`;
-        previewPdate.textContent = `${this.date}`;
-        previewPdescription.textContent = `${this.description}`;
-    }
-}
-
-const laskowice311218 = new PreviewCity('Jelcz-Laskowice', '31.12.2018r.', 'IV TOYOTA Bieg Sylwestrowy')
-laskowice311218.addPreview()
-
-const sobotka230319 = new PreviewCity('Sobótka', '23.03.2019r.', '12. PANAS Pólmaraton Ślężański')
-sobotka230319.addPreview()
-
-const krakow240319 = new PreviewCity('Kraków', '24.03.2019r.', '16 Krakowski Półmaraton Marzanny')
-krakow240319.addPreview()
-
-const warszawa310319 = new PreviewCity('Warszawa', '31.03.2019r.', '14 Półmaraton Warszawski')
-warszawa310319.addPreview()
-
-const debno070419 = new PreviewCity('Dębno', '07.04.2019r.', '46. Maraton Dębno')
-debno070419.addPreview()
-
-const gdansk140419 = new PreviewCity('Gdańsk', '14.04.2019r.', '5. Gdańsk Maraton')
-gdansk140419.addPreview()
-
-const warszawa140419 = new PreviewCity('Warszawa', '14.04.2019r.', 'Orlen Warsaw Marathon')
-warszawa140419.addPreview()
-
-const poznan140419 = new PreviewCity('Poznań', '14.04.2019r.', '12.PKO Poznań Pólmaraton')
-poznan140419.addPreview()
-
-const krakow280419 = new PreviewCity('Kraków', '28.04.2019r.', '18. Cracovia Maraton')
-krakow280419.addPreview()
-
-const poznan050519 = new PreviewCity('Poznań', '05.05.2019r.', 'Wings For Life WorldRun')
-poznan050519.addPreview()
-
-const wroclaw150619 = new PreviewCity('Wrocław', '15.06.2019r.', '7. PKO Nocny Wrocław Półmaraton')
-wroclaw150619.addPreview()
-
-const olawa010919 = new PreviewCity('Oława', '01.09.2019r.', 'XII Bieg Koguta')
-olawa010919.addPreview()
-
-const wroclaw150919 = new PreviewCity('Wrocław', '15.09.2019r.', '37. PKO Wrocław Maraton')
-wroclaw150919.addPreview()
-
-const warszawa290919 = new PreviewCity('Warszawa', '29.09.2019r.', 'PZU Maraton Warszawski')
-warszawa290919.addPreview()
-
-const katowice061019 = new PreviewCity('Katowice', '06.10.2019r.', '11. PKO Silesia Marathon')
-katowice061019.addPreview()
-
-const poznan201019 = new PreviewCity('Poznań', '20.10.2019r.', '20. PKO Poznań Maraton')
-poznan201019.addPreview()
 
 
 
