05ca9b2fee49d36994b5fc57bf3c3f9476e1a053
MorozMichal <moroz_michal@o2.pl>
2019-03-11 23:49:53 +0100

    work footer section

--- js/footer.js
+++ js/footer.js
@@ -9,3 +9,29 @@ window.addEventListener('scroll', () => {
         footerLogo.style.animation = ""
     }
 })
+
+const btnFooter = document.querySelector('.footer-contact-form-send');
+const inputName = document.querySelector('.footer-contact-form-name');
+const inputMail = document.querySelector('.footer-contact-form-mail');
+const inputTextArea = document.querySelector('.footer-contact-form-text')
+
+btnFooter.addEventListener("click", () => {
+    console.log("działa")
+    if (inputName.value === "") {
+        inputName.style.border = "1px solid red"
+    } else if (inputName.value !== "") {
+        inputName.style.border = "none"
+    }
+
+    if (inputMail.value === "") {
+        inputMail.style.border = "1px solid red"
+    } else if (inputMail.value !== "") {
+        inputMail.style.border = "none"
+    }
+
+    if (inputTextArea.value === "") {
+        inputTextArea.style.border = "1px solid red"
+    } else if (inputTextArea.value !== "") {
+        inputTextArea.style.border = "none"
+    }
+})
