5179d5abd66393ca69fbc17465f4ccf0c55d69b7
MorozMichal <34334015+MorozMichal@users.noreply.github.com>
2019-03-12 10:15:19 +0100

    work footer section add script valid form

--- js/footer.js
+++ js/footer.js
@@ -10,28 +10,17 @@ window.addEventListener('scroll', () => {
     }
 })
 
+//walidacja formularza kontaktowego
 const btnFooter = document.querySelector('.footer-contact-form-send');
-const inputName = document.querySelector('.footer-contact-form-name');
-const inputMail = document.querySelector('.footer-contact-form-mail');
-const inputTextArea = document.querySelector('.footer-contact-form-text')
+const footerInputs = document.querySelectorAll('.footer-input') //utworzyć własny atrybut
 
-btnFooter.addEventListener("click", () => {
-    console.log("działa")
-    if (inputName.value === "") {
-        inputName.style.border = "1px solid red"
-    } else if (inputName.value !== "") {
-        inputName.style.border = "none"
-    }
-
-    if (inputMail.value === "") {
-        inputMail.style.border = "1px solid red"
-    } else if (inputMail.value !== "") {
-        inputMail.style.border = "none"
-    }
-
-    if (inputTextArea.value === "") {
-        inputTextArea.style.border = "1px solid red"
-    } else if (inputTextArea.value !== "") {
-        inputTextArea.style.border = "none"
-    }
+btnFooter.addEventListener("click", (e) => {
+    e.preventDefault();
+    footerInputs.forEach((footerInput) => {
+        if (footerInput.value === "") {
+            footerInput.style.border = "1px solid red"
+        } else if (footerInput.value !== "") {
+            footerInput.style.border = "none"
+        }
+    })
 })
