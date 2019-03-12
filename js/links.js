58a959d8ea6dfdebbc61f2e09ec4210871512f1c
MorozMichal <moroz_michal@o2.pl>
2019-03-12 23:30:45 +0100

    recode section links

---
+++ js/links.js
@@ -0,0 +1,17 @@
+const links = document.querySelectorAll(".link-element");
+const linksPosition = document.querySelector(".links").offsetTop;
+
+//uruchamianie opóżniania
+window.addEventListener('scroll', () => {
+    if (scrollY >= linksPosition - 200) {
+        for (let i = 0; i < links.length; i++) {
+            setTimeout(() => {
+                links[i].style.opacity = "1"
+                links[i].style.transition = "1s"
+                console.log(i)
+            }, i * 700 + 700);
+        }
+
+
+    }
+})
