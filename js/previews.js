795709bdd5b8c81e3c8d21bed897d321c42686c2
MorozMichal <34334015+MorozMichal@users.noreply.github.com>
2019-03-27 12:56:42 +0100

    start work content section previews

--- js/previews.js
+++ js/previews.js
@@ -1,9 +1,12 @@
 class PreviewCity {
-  constructor(city, date, description, regulations) {
+  constructor(city, date, description, webOrganizer, regulations, records, limit) {
     this.city = city;
     this.date = date;
     this.description = description;
+    this.webOrganizer = webOrganizer;
     this.regulations = regulations;
+    this.records = records;
+    this.limit = limit
 
     this.previewsAll = document.querySelector(".section-previews-all");
     this.previewsContents = document.querySelector(".section-previews-content");
@@ -28,7 +31,10 @@ class PreviewCity {
       `<p>${this.city}</p>
     <p>${this.date}</p>
     <p>${this.description}</p>
-   <a href="${this.regulations}" target="_blank">link do regulaminu</a>`;
+    <p><a href="${this.webOrganizer}" target="_blank">Strona organizatora</a></p>
+   <p><a href="${this.regulations}" target="_blank">Regulamin</a></p>
+   <p><a href="${this.records}" target="_blank">Zapisy</a></p>
+   <p>limit uczestników: ${this.limit}</p>`;
   }
 }
 
@@ -38,7 +44,10 @@ const laskowice311218 = new PreviewCity(
   (city = "Jelcz-Laskowice"),
   (date = "31.12.2018r."),
   (description = "IV TOYOTA Bieg Sylwestrowy"),
-  (regulations = "http://www.endomondo.com")
+  (webOrganizer = "http://www.endomondo.com"),
+  (regulations = "http://www.endomondo.com"),
+  (records = "http://www.endomondo.com"),
+  (limit = "300")
 );
 
 // const laskowice311218 = new PreviewCity('Jelcz-Laskowice', '31.12.2018r.', 'IV TOYOTA Bieg Sylwestrowy', 'Jelcz-Laskowice')
@@ -49,7 +58,10 @@ const sobotka230319 = new PreviewCity(
   "Sobótka",
   "23.03.2019r.",
   "12. PANAS Pólmaraton Ślężański",
-  "http://www.wp.com"
+  "http://www.wp.com",
+  "http://www.wp.com",
+  "http://www.endomondo.com",
+  "500"
 );
 arrayPreview.push(sobotka230319);
 // sobotka230319.addPreview()
@@ -58,7 +70,10 @@ const krakow240319 = new PreviewCity(
   "Kraków",
   "24.03.2019r.",
   "16 Krakowski Półmaraton Marzanny",
-  "http://www.wp.com"
+  "http://www.wp.com",
+  "http://www.wp.com",
+  "http://www.endomondo.com",
+  "500"
 );
 arrayPreview.push(krakow240319);
 // krakow240319.addPreview()
@@ -67,7 +82,10 @@ const warszawa310319 = new PreviewCity(
   "Warszawa",
   "31.03.2019r.",
   "14 Półmaraton Warszawski",
-  "http://www.wp.com"
+  "http://www.wp.com",
+  "http://www.wp.com",
+  "http://www.endomondo.com",
+  "500"
 );
 arrayPreview.push(warszawa310319);
 // warszawa310319.addPreview()
@@ -76,7 +94,10 @@ const debno070419 = new PreviewCity(
   "Dębno",
   "07.04.2019r.",
   "46. Maraton Dębno",
-  "http://www.wp.com"
+  "http://www.wp.com",
+  "http://www.wp.com",
+  "http://www.endomondo.com",
+  "500"
 );
 arrayPreview.push(debno070419);
 // debno070419.addPreview()
@@ -85,7 +106,10 @@ const gdansk140419 = new PreviewCity(
   "Gdańsk",
   "14.04.2019r.",
   "5. Gdańsk Maraton",
-  "http://www.wp.com"
+  "http://www.wp.com",
+  "http://www.wp.com",
+  "http://www.endomondo.com",
+  "500"
 );
 arrayPreview.push(gdansk140419);
 // gdansk140419.addPreview()
@@ -94,7 +118,10 @@ const warszawa140419 = new PreviewCity(
   "Warszawa",
   "14.04.2019r.",
   "Orlen Warsaw Marathon",
-  "http://www.wp.com"
+  "http://www.wp.com",
+  "http://www.wp.com",
+  "http://www.endomondo.com",
+  "500"
 );
 arrayPreview.push(warszawa140419);
 // warszawa140419.addPreview()
@@ -103,7 +130,10 @@ const poznan140419 = new PreviewCity(
   "Poznań",
   "14.04.2019r.",
   "12.PKO Poznań Pólmaraton",
-  "http://www.wp.com"
+  "http://www.wp.com",
+  "http://www.wp.com",
+  "http://www.endomondo.com",
+  "500"
 );
 arrayPreview.push(poznan140419);
 // poznan140419.addPreview()
@@ -112,7 +142,10 @@ const krakow280419 = new PreviewCity(
   "Kraków",
   "28.04.2019r.",
   "18. Cracovia Maraton",
-  "http://www.wp.com"
+  "http://www.wp.com",
+  "http://www.wp.com",
+  "http://www.endomondo.com",
+  "500"
 );
 arrayPreview.push(krakow280419);
 // krakow280419.addPreview()
@@ -121,7 +154,10 @@ const poznan050519 = new PreviewCity(
   "Poznań",
   "05.05.2019r.",
   "Wings For Life WorldRun",
-  "http://www.wp.com"
+  "http://www.wp.com",
+  "http://www.wp.com",
+  "http://www.endomondo.com",
+  "500"
 );
 arrayPreview.push(poznan050519);
 // poznan050519.addPreview()
@@ -130,7 +166,10 @@ const wroclaw150619 = new PreviewCity(
   "Wrocław",
   "15.06.2019r.",
   "7. PKO Nocny Wrocław Półmaraton",
-  "http://www.wp.com"
+  "http://www.wp.com",
+  "http://www.wp.com",
+  "http://www.endomondo.com",
+  "500"
 );
 arrayPreview.push(wroclaw150619);
 // wroclaw150619.addPreview()
@@ -139,7 +178,10 @@ const olawa010919 = new PreviewCity(
   "Oława",
   "01.09.2019r.",
   "XII Bieg Koguta",
-  "http://www.wp.com"
+  "http://www.wp.com",
+  "http://www.wp.com",
+  "http://www.endomondo.com",
+  "500"
 );
 arrayPreview.push(olawa010919);
 // olawa010919.addPreview()
@@ -148,7 +190,10 @@ const wroclaw150919 = new PreviewCity(
   "Wrocław",
   "15.09.2019r.",
   "37. PKO Wrocław Maraton",
-  "http://www.wp.com"
+  "http://www.wp.com",
+  "http://www.wp.com",
+  "http://www.endomondo.com",
+  "500"
 );
 arrayPreview.push(wroclaw150919);
 // wroclaw150919.addPreview()
@@ -157,7 +202,10 @@ const warszawa290919 = new PreviewCity(
   "Warszawa",
   "29.09.2019r.",
   "PZU Maraton Warszawski",
-  "http://www.wp.com"
+  "http://www.wp.com",
+  "http://www.wp.com",
+  "http://www.endomondo.com",
+  "500"
 );
 arrayPreview.push(warszawa290919);
 // warszawa290919.addPreview()
@@ -166,7 +214,10 @@ const katowice061019 = new PreviewCity(
   "Katowice",
   "06.10.2019r.",
   "11. PKO Silesia Marathon",
-  "http://www.wp.com"
+  "http://www.wp.com",
+  "http://www.wp.com",
+  "http://www.endomondo.com",
+  "500"
 );
 arrayPreview.push(katowice061019);
 // katowice061019.addPreview()
@@ -175,7 +226,10 @@ const poznan201019 = new PreviewCity(
   "Poznań",
   "20.10.2019r.",
   "20. PKO Poznań Maraton",
-  "http://www.wp.com"
+  "http://www.wp.com",
+  "http://www.wp.com",
+  "http://www.endomondo.com",
+  "500"
 );
 arrayPreview.push(poznan201019);
 // poznan201019.addPreview()
